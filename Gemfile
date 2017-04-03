b69fa0055c84953a274a3620e5ca39088b38d516
takaya <tky.c10ver@gmail.com>
2017-04-04 01:10:25 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'mechanize'
+
+# Development & Test requirements
+group :development, :test do
+  gem 'pry'
+end
---
+++ script/get.rb
@@ -0,0 +1,33 @@
+require 'mechanize'
+require 'json'
+require './lib/prefecture'
+
+SITE_URL = 'http://www.mhlw.go.jp/stf/seisakunitsuite/bunya/koyou_roudou/roudoukijun/minimumichiran/'
+TR_COUNT = 49  # 47都道府県 + タイトル行 + フッター行
+SAVE_FILE = 'docs/minimum_wage.json'
+
+agent = Mechanize.new
+page = agent.get(SITE_URL)
+
+# HTML内にtableが1つだけあり、行(tr)が49つ(47都道府県 + タイトル行 + フッター行)あることを前提とする
+# 49行ではなかった場合はエラーとして終了
+tr_list = page.search('table tr')
+exit false unless tr_list.length === 49
+
+minimum_wages = {}
+
+# 先頭行はタイトル行とし、読み捨てる
+tr_list.shift
+# 最終行はフッター行とし、読み捨てる
+tr_list.pop
+
+tr_list.each do |tr|
+  td_list = tr.search('td')
+  prefecture_name = Prefecture.convert(td_list.first.inner_text)
+  wage = td_list[1].inner_text.gsub(/(\s|　|,)+/, '').to_i
+  minimum_wages[prefecture_name] = wage
+end
+
+File.open(SAVE_FILE, 'w') do |file|
+  JSON.dump(minimum_wages, file)
+end
